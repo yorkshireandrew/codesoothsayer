712504944b86ef72740593de9a7cd8e0f6de59f8
andrew pate <andrewpate08@hotmail.com>
2018-02-20 19:23:08 +0000

    Using a template literal for content

--- guru/WebContent/gurucontent.js
+++ guru/WebContent/gurucontent.js
@@ -2,9 +2,14 @@ var content = [
 	["number 1", "is the first number", "", true, false],
 	["number 2", "is definately the second number", "", true, false],
 	["number 3", "\"what's that\" he cried \n\nxxx"
-		+ "\"I've no idea & shut up\" <<<moose>>> replied with a &&", "", true,false]
+		+ "\"I've no idea & shut up\" <<<moose>>> replied with a &&", "", true,false],
 	//["number 3", "\"what's that\" he cried \n"
 	//	+ ""\"I've no idea & shut up\"<<moose>> replied"+ 
 	//		"\n\n" +
 	//		"then promptly sneezed", "", true, false]
+		["number 4", 
+`number four uses
+a template
+literal`, "", true,false]
+
 ]
